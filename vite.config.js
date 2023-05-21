5217076be3c10ab349c4c39ae47118becc313431
phunghkt0 <phunghkt0@gmail.com>
2023-05-21 22:15:42 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
     resolve: {
         alias: {
-            "@": resolve(_dirname, "src"),
+            "@": resolve(__dirname, "src"),
         },
     }, 
 });
